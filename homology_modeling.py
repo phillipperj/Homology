c6851a23bee4e8281be9c40808423cc238154d37
Phillippe <phillipperj@gmail.com>
2014-01-24 14:02:27 -0200

    get_homologs() is working fine. get_motifs() has some problems.

--- homology_modeling.py
+++ homology_modeling.py
@@ -1,36 +1,91 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 22 11:24:53 2014
-@author: phillippe
-All other scripts need to be in the same folder.
-
-Usage: phython homology_modeling.py file.fasta 
-"""
-import sys
-from Bio import SeqIO as Seq
 from modeller import *
 from modeller.automodel import *
+import sys, subprocess, shlex
 
-def get_sequences():
-    #read from an input fasta file a protein sequence.
-    try:
-        Ifile = Seq.parse(sys.argv[1],'fasta', alphabet)
-    except:
-        """ Usage: phython homology_modeling.py file.fasta 
-        """
-def get_motifs():
-    #search in the Pfam database for known motifs.
-    #The output isn't parsed though.
-    pass
+USAGE = """
+Created on Wed Jan 22 11:24:53 2014
+@author: phillippe
+
+Usage: phython homology_modeling.py file.fasta
+
+Files pdball.pir or pdb_95.pir are needed for database search.
+"""
+DATABASE_TYPE = 'pdball.pir'
+
+#def get_sequences():
+#    #read from an input fasta file a protein sequence.
+#    try:
+#        Ifile = Seq.parse(sys.argv[1],'fasta', ProteinAlphabet())
+#    except:
+#        print usage
+print sys.argv
+
+if len(sys.argv) < 2:
+    print USAGE
 
 def get_homologs():
-    #search for homologs in the non-redundant (with 95% identity cutoff) PDB.
-    pass
+    """
+    search for homologs in the non-redundant (with 95% identity cutoff) PDB or
+    the complete PDB on the DATABASE_TYPE variable.
+    """
+    
+    log.verbose()
+    env = environ()
+
+    # Read in the database of PDB chains clustered at 95% sequence identity
+    sdb = sequence_db(env)
+    try:
+        sdb.read(seq_database_file=DATABASE_TYPE, seq_database_format='PIR',
+                 chains_list='ALL', minmax_db_seq_len=(30, 4000),
+                 clean_sequences=True)
+    except IOError:
+        errmsg = str(sys.exc_info()[1]) + """
+       Could not open the PDB 95 database or PDBALL. This database is *not* 
+       included in the Modeller release, since it is frequently updated. 
+       To run this, you first need to download pdb_95.pir.gz from the "Data 
+       file downloads" page on the Modeller website.
+    """
+        print(errmsg)
+        sys.exit(0)
+
+    # Read in the target sequence in PIR alignment format
+    aln = alignment(env)
+    aln.append(file=sys.argv[1], alignment_format='PIR', align_codes='ALL')
+
+    # Convert the target sequence from alignment to profile format
+    prf = aln.to_profile()
+
+    # Scan sequence database to pick up homologous sequences
+    prf.build(sdb, matrix_offset=-450, rr_file='${LIB}/blosum62.sim.mat',
+              gap_penalties_1d=(-500, -50), n_prof_iterations=1,
+              check_profile=False, max_aln_evalue=0.01, gaps_in_target=True)
+
+    # Write out the profile in text format
+    prf.write(file='search_' + sys.argv[1].strip('.pir') + '_pdBall.prf',
+              profile_format = 'TEXT')
+    
+    aln2 = prf.to_alignment()
+    aln2.write(file = 'search_' + sys.argv[1].strip('.pir') + '_pdBall.pir', 
+              alignment_format='PIR')
+    aln2.write(file = 'search_' + sys.argv[1].strip('.pir') + '_pdBall.fasta', 
+              alignment_format='FASTA')
+
+def get_motifs():
+    """
+    search in the Pfam database for known motifs.
+    The output isn't parsed though.
+    """
+    args = shlex.split("curl -L -H 'Expect:' -H 'Accept:text/xml' -F hmmdb=pfam -F \
+    seq='<" + sys.argv[1] + " http://hmmer.janelia.org/search/hmmscan")
+    subprocess.Popen(args)
 
 def do_model():
-    
+
     pass
 
 def do_assessment():
     pass
+
+#get_homologs()
+get_motifs()
